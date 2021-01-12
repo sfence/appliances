c962d06c7a9dc67f9aea1ec5e348d051e659c6f5
SFENCE <sfence.software@gmail.com>
2021-01-12 20:48:25 +0100

    Version for Hades Revisited.

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ local modpath = minetest.get_modpath(minetest.get_current_modname());
 
 appliances.have_mesecons = minetest.get_modpath("mesecons")~=nil;
 appliances.have_pipeworks = minetest.get_modpath("pipeworks")~=nil;
-appliances.have_technic = minetest.get_modpath("technic")~=nil;
+appliances.have_technic = minetest.get_modpath("hades_technic")~=nil;
 
 dofile(modpath.."/functions.lua");
 dofile(modpath.."/appliance.lua");
